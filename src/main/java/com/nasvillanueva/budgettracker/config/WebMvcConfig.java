ea52b72287ebd06e284a35dc254cd422004ff92c
Nas Villanueva <jvillanueva@exist.com>
2015-12-20 23:14:51 +0800

    Added mapping for admin angular app and bower_components

--- src/main/java/com/nasvillanueva/budgettracker/config/WebMvcConfig.java
+++ src/main/java/com/nasvillanueva/budgettracker/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -17,11 +18,18 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**", "/images/**", "/js/**")
-                .addResourceLocations("/resources/css/", "/resources/images/", "/resources/js/");
-        registry.addResourceHandler("/swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/css/**", "/images/**", "/js/**", "/bower_components/**",
+                        "/swagger-ui.html", "/webjars/**")
+                .addResourceLocations("/css/", "/images/", "/js/", "/bower_components/",
+                        "classpath:/META-INF/resources/", "classpath:/META-INF/resources/webjars/");
+
+        registry.addResourceHandler("/admin_app/**")
+                .addResourceLocations("/admin_app/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/admin/").setViewName("/admin_app/index.html");
+        registry.addRedirectViewController("/admin", "/admin/");
     }
 }
